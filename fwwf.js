910d2f4fc7123f4634adf2510720fa6672e9fa7b
trinajpng2210 <trina.jp.ng@gmail.com>
2014-12-12 20:45:08 +1100

    12122014 Commits
    Commits with all pages now set up.

--- fwwf.js
+++ fwwf.js
@@ -3,7 +3,7 @@ var api_data;
 
 $(document).ready(function() {
 
-  var image1 = $('#california-photo');
+  var image1 = $('#section-surf');
   var image2 = $('#section-coffee');
   var body = $('body')
   $(document).scroll(function() {
@@ -19,32 +19,62 @@ $(document).ready(function() {
     }
   });
 
+  // Instagram feed
   $.ajax({
     type: 'GET',
     dataType: 'jsonp',
-    url: 'https://api.instagram.com/v1/tags/westcoastbestcoast/media/recent?callback=?&count=36&access_token=419417011.cfa3287.2fd7043455f143dab131afb7fb6ce35e',
+    url: 'https://api.instagram.com/v1/users/419417011/media/recent?callback=?&access_token=419417011.cfa3287.2fd7043455f143dab131afb7fb6ce35e',
     success: function(response) {
-      api_data = response,
+      api_data = response;
       console.log(response)
-    //   for (var i = 0; i < 36; i++) {
-    //     $('#instafeed ul').append('<li><a target="_blank" href="' + response.data[i].link + '"><img scr="' + response.data[i].images.standard_resolution.url + '"></img></a></li>'),
-    //   }
+      for (var i = 0; i < response.data.length; i++) {
+        console.log(response.data[i].length);
+        console.log(response.data[i].link);
+        console.log(response.data[i].images.standard_resolution.url);
+        $('#instafeed ul').append('<li><a target="_blank" href="' + response.data[i].link + '"><img src="' + response.data[i].images.standard_resolution.url + '"></img></a></li>');
+      }
     }
   });
 
   // Email form
-  var firebaseEmailAddresses = new Firebase('https://fewd-dev.firebaseio-demo.com/emailaddresses');
+  var firebaseEmailAddresses = new Firebase('https://f5snupukwtz.firebaseio-demo.com/');
+  $('.emailForm-submit').onclick(function () {
+    var emailAddress = $('.newEmailAddress').val();
+    firebaseEmailAddresses.push( {email: emailAddress} );
+    $('.newEmailAddress').val("");
+    return false;
+  })
   emailAddresses.on('child_added', function(snapshot) {
     var emailAddress = snapshot.val();
     console.log(emailAddress);
   });
 
-  $('.emailForm').submit(function() {
-    var newEmailAddress = $('.newEmailAddress').val();
-    var addedEmailAddress = { text: newEmailAddress };
-    firebaseEmailAddresses.push(addedEmailAddress);
-    $('.newEmailAddress').val("");
-    return false;
+  // Photography gallery
+  $('#filters').on('click', 'li.beach-filter', function() {
+    console.log('Beach filter clicked!');
+    $('img.beach-filter').show();
+    $('img.cafe-filter').hide();
+  });
+  $('#filters').on('click', '.cafe-filter', function() {
+    $('img.cafe-filter').show();
+    $('img.beach-filter').hide();
+  });
+  $('#filters').on('click', '.australia-filter', function() {
+    $('img.australia-filter').show();
+    $('img.california-filter').hide();
+  });
+  $('#filters').on('click', '.california-filter', function() {
+    $('img.california-filter').show();
+    $('img.australia-filter').hide();
   });
 
+  // Contact form
+  var dataRef = new Firebase('https://rmhhct2pjdu.firebaseio-demo.com/');
+  $('#contact-form').on('submit', function() {
+    var name = $('#name').val();
+    // var action = How do I choose the radio button checked?
+    dataRef.push( {name: name, action: action} );
+    $('#name').val("");
+    return false;
+  });
 });
